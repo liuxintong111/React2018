45252082fd044d324748373b7a3a86d9057617a4
liuxintong111 <1544158668@qq.com>
2020-10-02 19:44:43 +0800

    ok

---
+++ precache-manifest.c92b8b592944b68bb3b2f8b0d210ecbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52146933312d4242f2cab9e628fbff35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8c9c95ec950e0c79b7a",
+    "url": "/static/js/2.b621457f.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/static/js/2.b621457f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "578fbbf22fa7507e14ea",
+    "url": "/static/js/main.45ab9ec3.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
